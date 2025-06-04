a76dfcba15eb447d160e84c614ebf16be50cbf49
Samuel Yudi <toyotaa038@gmail.com>
2025-06-03 21:23:40 -0300

    Add Person class and create_person_list function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
+from typing import List
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: List[dict]) -> List[Person]:
+    person_list: List[Person] = []
+
+    # Cria as instâncias sem relacionamentos
+    for person_data in people:
+        person_instance = Person(person_data["name"], person_data["age"])
+        person_list.append(person_instance)
+
+    # Conecta os relacionamentos wife/husband
+    for person_data in people:
+        person_instance = Person.people[person_data["name"]]
+
+        if "wife" in person_data and person_data["wife"] is not None:
+            person_instance.wife = Person.people[person_data["wife"]]
+
+        elif "husband" in person_data and person_data["husband"] is not None:
+            person_instance.husband = Person.people[person_data["husband"]]
+
+    return person_list
